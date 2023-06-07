89d4c51df8641fdfc3861315c9c562308e548d4d
AmexTevz <amextevz@yahoo.com>
2023-06-07 11:02:11 -0400

    modified

--- Project_Files/main.py
+++ Project_Files/main.py
@@ -7,12 +7,12 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-Apkudo = '#B20600'
+back_color = '#B20600'
 FONT_NAME = "Courier"
 font_2 = 'Rage Italic'
 
 
-def reset(reset_text,header):
+def reset(reset_text, header):
     info.config(state='normal')
     info.delete('1.0', END)
     info.insert(END, reset_text)
@@ -22,7 +22,7 @@ def reset(reset_text,header):
     info.config(state='disabled')
 
 
-def charge1(text_version,desc):
+def charge1(text_version, desc):
     info.config(state='normal')
     info.delete('1.0', END)
     info.insert(END, text_version)
@@ -33,66 +33,45 @@ def charge1(text_version,desc):
     description['text'] = desc
 
 
+def button(title, chapter, header, w=None, h=None, background=None):
+    Button(text=title, highlightbackground=YELLOW, width=w, height=h, fg=background, highlightthickness=0,
+           command=lambda: charge1(chapter, header)).pack(side=LEFT, padx=50)
+
 
 def choice(*args):
     if clicked.get() == 'USB-C':
-        reset(text.usb_1,'USB-C')
+        reset(text.usb_1, 'USB-C')
         add_message()
         for i in window.winfo_children()[5:]:
             i.forget()
 
-        Button(text="Overview", highlightbackground=YELLOW, highlightthickness=0, width=8, command= lambda: charge1(text.usb_2,'overview')).pack(
-            side=LEFT,
-            padx=50)
-        Button(text="PD", highlightbackground=YELLOW, highlightthickness=0, width=8, command = lambda: charge1(text.power_delivery,'Power Delivery')).pack(
-            side=LEFT,
-            padx=60)
+        button('Overview', text.usb_2, 'overview', 8)
 
-        Button(text="Reset", fg=Apkudo, width=5, height=3, highlightbackground=YELLOW, command = lambda: reset(text.usb_1,'USB-C')).pack(
-            side=LEFT,
-            padx=60)
+        button('PD', text.power_delivery, 'Power Delivery', 8)
 
-        Button(text="VO", highlightbackground=YELLOW, highlightthickness=0, width=8, command= lambda: charge1(text.video_output,'Video Output')).pack(
-            side=LEFT,
-            padx=60)
+        button('Rest', text.usb_1, 'USB-C', 5, 3, back_color)
 
-        Button(text="Downsides", highlightbackground=YELLOW, highlightthickness=0, width=8, command=  lambda: charge1(text.downsides,'Downsides')).pack(
-            side=LEFT,
-            padx=50)
+        button('VO', text.video_output, 'Video Output', 8)
+
+        button('Downsides', text.downsides, 'Downsides', 8)
 
     else:
         if clicked.get() == 'Charging':
-            reset(text.summary,'Charging Standards')
+            reset(text.summary, 'Charging Standards')
             for i in window.winfo_children()[5:]:
                 i.forget()
 
-            Button(window, text="QC 1.0", highlightbackground=YELLOW, highlightthickness=0, width=7,
-                   command= lambda: charge1(text.charge_1,'Quick Charge 1.0')).pack(
-                side=LEFT,
-                padx=30)
-            Button(text="QC 2.0", highlightbackground=YELLOW, highlightthickness=0, width=7, command=lambda: charge1(text.charge_2,'Quick Charge 2.0')).pack(
-                side=LEFT,
-                padx=30)
+            button('QC 1.0', text.charge_1, 'Quick Charge 1.0', 8)
 
-            Button(text="QC 3.0", highlightbackground=YELLOW, highlightthickness=0, width=7, command= lambda: charge1(text.charge_3,'Quick Charge 3.0')).pack(
-                side=LEFT,
-                padx=30)
-            Button(text="Reset", fg=Apkudo, width=5, height=3, highlightbackground=YELLOW, command=lambda: reset(text.summary,'Charging Standards')).pack(
-                side=LEFT,
-                padx=30)
+            button('QC 3.0', text.charge_3, 'Quick Charge 3.0', 8)
 
-            Button(text="QC 4.0", highlightbackground=YELLOW, highlightthickness=0, width=7, command=lambda: charge1(text.charge_4,'Quick Charge 4.0')).pack(
-                side=LEFT,
-                padx=30)
+            button('Reset', text.summary, 'Charging Standards', 5, 3, back_color)
 
-            Button(text="QC 4.0+", highlightbackground=YELLOW, highlightthickness=0, width=7,
-                   command=lambda: charge1(text.charge_4_1,'Quick Charge 4.0+')).pack(
-                side=LEFT,
-                padx=30)
+            button('QC 4.0', text.charge_4, 'Quick Charge 4.0', 8)
 
-            Button(text="QC 5.0", highlightbackground=YELLOW, highlightthickness=0, width=7, command=lambda: charge1(text.charge_5,'Quick Charge 5.0')).pack(
-                side=LEFT,
-                padx=30)
+            button('QC 4.0+', text.charge_4_1, 'Quick Charge 4.0+', 8)
+
+            button('QC 5.0', text.charge_5, 'Quick Charge 5.0', 8)
 
 
 window = Tk()
@@ -100,7 +79,7 @@ window.title('NR Project')
 window.config(padx=10, pady=5, bg=YELLOW)
 window.geometry('1100x600')
 
-apkudo = Label(text='Apkudo - NR Team', font=(FONT_NAME, 18), fg=Apkudo, bg=YELLOW, pady=10)
+apkudo = Label(text='Apkudo - NR Team', font=(FONT_NAME, 18), fg=back_color, bg=YELLOW, pady=10)
 apkudo.pack()
 
 options = ['Charging', 'USB-C']
@@ -120,13 +99,13 @@ description.pack()
 info = Text(window, height=12, width=150, bg=YELLOW, highlightthickness=0, fg='black', font=(FONT_NAME, 18))
 info.pack()
 info.insert(END, text.summary)
-info.tag_config('color', foreground=Apkudo)
+info.tag_config('color', foreground=back_color)
 info['wrap'] = WORD
 info.get("1.0", END)
 
 
 def add_message():
-    info.insert(END, '\n\n\n\t\t\t\tHit any button for more information','color')
+    info.insert(END, '\n\n\n\t\t\t\tHit any button for more information', 'color')
 
 
 add_message()
